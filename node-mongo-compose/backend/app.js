2b8a89f5e4ed68ded2c800d9a0477d91e75829bc
Marcos Gregorio <marquinhogreg2013@gmail.com>
2024-05-15 09:42:08 -0300

    fazendo a aplicacao web

--- node-mongo-compose/backend/app.js
+++ node-mongo-compose/backend/app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const restful = require('node-restful')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 const server = express()
 const mongoose = restful.mongoose
 
@@ -7,5 +9,22 @@ mongoose.Promise = global.Promise
 mongoose.connect('mongodb://db/mydb')
 
 server.get('/', (req, res, next) => res.send('Backend'))
+
+
+// midlewares
+
+server.use(bodyParser.urlencoded({entended: true}))
+server.use(bodyParser.json())
+server.use(cors())
+
+// ODM
+const Client = restful.model('CLient', {
+	name: { type: String, required: true }
+})
+
+Client.methods(['get', 'post', 'put', 'delete'])
+Client.updateOptions({new: true, runValidators: true})
+
+Client.register(server, '/clients')
 
 server.listen(3000)
